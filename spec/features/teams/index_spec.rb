747e123aa532fab8d119b4c6c1aaff3a3a92f944
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-01 17:25:37 -0600

    Add user can see all players test to Team_index_spec

--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'teams index page', type: :feature do
     @team_1 = Team.create(name: "Secret", age: 4, location: "Europe")
     @team_2 = Team.create(name: "Natus Vincere", age: 8, location: "Ukraine")
 
+    @puppey = @team_1.players.create(name: "Puppey", winrate: 0.666)
+    @chu = @team_2.players.create(name: "Chu", winrate: 0.521)
+
     visit "/teams"
   end
 
@@ -18,6 +21,17 @@ RSpec.describe 'teams index page', type: :feature do
       expect(page).to have_content(@team_2.name)
       expect(page).to have_content("Age: #{@team_2.age}")
       expect(page).to have_content("Location: #{@team_2.location}")
+    end
+  end
+
+  it 'user can see all players' do
+    within "team_#{@team_1.id}" do
+      expect page.to have_content "Name: #{@puppey.name}"
+      expect page.to have_content "Winrate: #{@puppey.winrate}"
+    end
+    within "team_#{@team_1.id}" do
+      expect page.to have_content "Name: #{@chu.name}"
+      expect page.to have_content "Winrate: #{@chu.winrate}"
     end
   end
 end
