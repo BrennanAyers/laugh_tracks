e25f71a4c984aa57760aa46ddff28f08a882acd4
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-30 18:10:34 -0600

    Refactor Teams model spec to use subject of described_class

--- spec/models/teams_spec.rb
+++ spec/models/teams_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
+  subject { described_class.new }
   it 'is valid with valid attributes' do
-    expect(Team.new(name: 'DotA')).to be_valid
+    subject.name = "DotA"
+    subject.age = 1
+    subject.location = "USA"
+    expect(subject).to be_valid
   end
 
   it 'is not valid without a name' do
-    team = Team.new(name: nil)
-    expect(team).to_not be_valid
+    expect(subject).to_not be_valid
   end
+  
   it 'is not valid without an age'
   it 'is not valid without a location'
 end
