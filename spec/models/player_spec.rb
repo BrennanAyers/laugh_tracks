c85cfe599ae0c3ac65be03a1b4b64908c6c7aacc
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-01 16:41:27 -0600

    Fix Player spec to have two assertions

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Player, type: :model do
 
   describe "validations" do
-    it {should validate_presence_of :name, :winrate}
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :winrate}
   end
 end
