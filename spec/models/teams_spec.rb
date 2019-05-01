ab4030559726feeea89f33ddefd29aeb41dd8fb4
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-01 16:46:06 -0600

    Refactor Teams model validations

--- spec/models/teams_spec.rb
+++ spec/models/teams_spec.rb
@@ -1,24 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  subject { described_class.new(name: "DotA", age: 1, location: "USA") }
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without an age' do
-    subject.age = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a location' do
-    subject.location = nil
-    expect(subject).to_not be_valid
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :age}
+    it {should validate_presence_of :location}
   end
 end
