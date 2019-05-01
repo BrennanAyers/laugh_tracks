26b0eca6f01a4d010276603322256ba9fc39852d
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-01 17:13:54 -0600

    Refactor Teams_index test to use before :each

--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'teams index page', type: :feature do
-  it 'user can see all teams' do
-    team_1 = Team.create(name: "Secret", age: 4, location: "Europe")
-    team_2 = Team.create(name: "Natus Vincere", age: 8, location: "Ukraine")
+  before :each do
+    @team_1 = Team.create(name: "Secret", age: 4, location: "Europe")
+    @team_2 = Team.create(name: "Natus Vincere", age: 8, location: "Ukraine")
 
     visit "/teams"
+  end
 
-    within "#team_#{team_1.id}" do
-      expect(page).to have_content(team_1.name)
-      expect(page).to have_content("Age: #{team_1.age}")
-      expect(page).to have_content("Location: #{team_1.location}")
+  it 'user can see all teams' do
+    within "#team_#{@team_1.id}" do
+      expect(page).to have_content(@team_1.name)
+      expect(page).to have_content("Age: #{@team_1.age}")
+      expect(page).to have_content("Location: #{@team_1.location}")
     end
-    within "#team_#{team_2.id}" do
-      expect(page).to have_content(team_2.name)
-      expect(page).to have_content("Age: #{team_2.age}")
-      expect(page).to have_content("Location: #{team_2.location}")
+    within "#team_#{@team_2.id}" do
+      expect(page).to have_content(@team_2.name)
+      expect(page).to have_content("Age: #{@team_2.age}")
+      expect(page).to have_content("Location: #{@team_2.location}")
     end
   end
 end
