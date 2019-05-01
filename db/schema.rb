6d9be6b62b2d7f431e5e9f91b299a4e5b28c9917
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-01 16:38:08 -0600

    Add Player migration file and update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190430225345) do
+ActiveRecord::Schema.define(version: 20190501222510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.float "winrate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
