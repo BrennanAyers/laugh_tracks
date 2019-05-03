e5f2e0a11a292e8b02f065746f38e9ea93c0a638
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-03 17:12:01 -0600

    Add default image value for Teams and Players added without a photo

--- db/migrate/20190502200025_add_image_to_teams.rb
+++ db/migrate/20190502200025_add_image_to_teams.rb
@@ -1,5 +1,5 @@
 class AddImageToTeams < ActiveRecord::Migration[5.1]
   def change
-    add_column :teams, :image, :string
+    add_column :teams, :image, :string, default: "default.png"
   end
 end
--- db/migrate/20190502200201_add_image_to_players.rb
+++ db/migrate/20190502200201_add_image_to_players.rb
@@ -1,5 +1,5 @@
 class AddImageToPlayers < ActiveRecord::Migration[5.1]
   def change
-    add_column :players, :image, :string
+    add_column :players, :image, :string, default: "default.png"
   end
 end
