157525c3908ea5be60f6381b2973fa03e169c8c3
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-04 14:22:32 -0600

    Add page info to teams/new spec

--- spec/features/teams/new_spec.rb
+++ spec/features/teams/new_spec.rb
@@ -4,16 +4,22 @@ RSpec.describe 'New Team', type: :feature do
   describe "As a user" do
     describe "When I visit the new Teams form" do
       it "can create a new Team" do
-          visit "/teams/new"
+        visit "/teams/new"
 
-          fill_in "name", with: "Test"
-          fill_in "age", with: 1
-          fill_in "location", with: "Test"
-          click_button "Submit"
+        fill_in "name", with: "Test"
+        fill_in "age", with: 1
+        fill_in "location", with: "Test"
+        click_button "Submit"
 
-          new_team = Team.last
+        new_team = Team.last
 
-          expect(current_path).to eq("/teams/#{new_team.id}")
+        expect(current_path).to eq("/teams")
+
+        within("team_#{new_team.id}") do
+          expect(page).to have_content(new_team.name)
+          expect(page).to have_content("Age: #{new_team.age}")
+          expect(page).to have_content("Location: #{new_team.location}")
+        end
       end
     end
   end
