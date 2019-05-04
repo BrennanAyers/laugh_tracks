c5e6a542cef165062045b8aa4e2f9a9e354c0c8b
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-03 18:08:43 -0600

    Add initial Team::by_age model test

--- spec/models/teams_spec.rb
+++ spec/models/teams_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Team, type: :model do
   describe "associations" do
     it {should have_many :players}
   end
+
+  describe "class methods" do
+    before :each do
+      @team_1 = Team.create(name: "Secret", age: 4, location: "Europe", image: "https://steamcdn-a.akamaihd.net/apps/dota2/images/team_logos/1838315.png")
+      @team_2 = Team.create(name: "Natus Vincere", age: 8, location: "Ukraine", image: "https://steamcdn-a.akamaihd.net/apps/dota2/images/team_logos/36.png")
+    end
+
+    it "should only return teams by age" do
+      expect(Team.by_age(4)).to eq(@team_1)
+    end
+  end
 end
