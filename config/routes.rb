ef98deda8c8896a1f20ee3b9405d8387ac990cde
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-04 16:29:58 -0600

    Add get/teams/:id route to teams#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/teams', to: 'teams#index'
   get 'teams/new', to: 'teams#new'
   post '/teams', to: 'teams#create'
+  get '/teams/:id', to: 'teams#show'
 end
