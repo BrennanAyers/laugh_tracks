578f54d86771277d61563bb224117f6c5326312c
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-05 12:47:17 -0600

    Add teams/index.html ivars for TotalPlayerCount and AvgPlayerWinrate

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,10 +4,14 @@ class TeamsController < ApplicationController
       @teams = Team.by_age(params[:age])
       @average_age = @teams.average_age
       @team_locations = @teams.locations
+      @total_player_count = @teams.total_player_count
+      @average_player_winrate = @teams.average_player_winrate
     else
       @teams = Team.all
       @average_age = Team.average_age
       @team_locations = Team.locations
+      @total_player_count = Team.total_player_count
+      @average_player_winrate = Team.average_player_winrate
     end
   end
 
